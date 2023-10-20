337c1c3045a7b25ea36f99e0cc00088dae6193ba
Ondřej <hruboson@gmail.com>
2023-10-20 09:49:42 +0200

    Revert "Added append_strinc_c function"
    This reverts commit 224830a7c60600cedce7bbe7ca1251c8dee9e2db.

--- src/string.c
+++ src/string.c
@@ -5,21 +5,21 @@
 void init_string(String *string) {
     if (string == NULL)
         return;
-
+    
     string->capacity = STR_INITIAL_CAPACITY;
     string->length = 0;
 
-    string->data = (char *)malloc(sizeof(char) * STR_INITIAL_CAPACITY);
+    string->data = (char *) malloc(sizeof(char) * STR_INITIAL_CAPACITY);
 
     if (string->data == NULL) {
         exit(99);
     }
 }
 
-void free_string(String *string) {
+void free_string (String *string) {
     if (string == NULL)
         return;
-
+    
     string->length = 0;
     string->capacity = 0;
 
@@ -27,13 +27,13 @@ void free_string(String *string) {
     string->data = NULL;
 }
 
-void append_string(String *string, const char *str_to_append) {
+void append_string(String * string, const char * str_to_append) {
     if (string == NULL)
         return;
-
+    
     for (int i = 0; str_to_append[i] != '\0'; i++) {
-        if (string->length + 1 > string->capacity) {  // pokud by se to po pridani znaku uz nevlezlo, zvetsi se 2x
-            char *new_data = (char *)realloc(string->data, sizeof(char) * string->capacity * 2);
+        if (string->length + 1 > string->capacity) { // pokud by se to po pridani znaku uz nevlezlo, zvetsi se 2x
+            char *new_data = (char *) realloc(string->data, sizeof(char) * string->capacity * 2);
 
             if (new_data == NULL) {
                 free_string(string);
@@ -48,21 +48,5 @@ void append_string(String *string, const char *str_to_append) {
     }
 }
 
-void append_string_c(String *string, char c) {
-    if (string == NULL)
-        return;
-
-    if (string->length + 2 > string->capacity) {
-        char *new_data = (char *)realloc(string->data, sizeof(char) * string->capacity * 2);
-        if (new_data == NULL) {
-            free_string(string);
-            exit(99);
-        }
-        string->data = new_data;
-
-        string->capacity *= 2;
-    }
-
-    string->data[(string->length)++] = c;
-    string->data[(string->length)] = '\0';
-}
+//TODO
+void append_string_c( String*, char );
--- tests/test_string.c
+++ tests/test_string.c
@@ -1,4 +1,5 @@
 #include "../include/string.h"
+
 #include "unity.h"
 
 String _string;
@@ -11,10 +12,6 @@ void setUp() {
 void tearDown() {
     free_string(string);
 }
-
-/**
- * Basic tests
- */
 
 void test_init_string(void) {
     TEST_ASSERT_NOT_NULL(string);
@@ -37,51 +34,21 @@ void test_free_string(void) {
 void test_append_string_to_empty(void) {
     char *appended_string = "test string";
     append_string(string, appended_string);
-
+    
     TEST_ASSERT_EQUAL_STRING(appended_string, string->data);
 }
 
-void test_append_string_char(void) {
-    append_string_c(string, 'c');
-    TEST_ASSERT_EQUAL_STRING("c", string->data);
-}
+// void test_append_string_char(void) {
+//     append_string_c(string, 'c');
+    
+//     TEST_ASSERT_EQUAL_STRING('c', string->data);
+// }
 
-/**
- * Advanced tests
- */
-void test_adv_append_long_string(void){
-    const char *appended_string_1 = "Well, the way they make shows is, they make one show.That show 's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they' re going to make more shows.Some pilots get picked and become television programs.Some don't, become nothing. She starred in one of the ones that became nothing.";
-    const char *appended_string_2 = "My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?";
-    append_string(string, appended_string_1);
-    append_string(string, appended_string_2);
-
-    TEST_ASSERT_EQUAL_STRING("Well, the way they make shows is, they make one show.That show 's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they' re going to make more shows.Some pilots get picked and become television programs.Some don't, become nothing. She starred in one of the ones that became nothing.My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?", string);
-}
-
-void test_adv_append_string_char(void) {
-    for(int i = 0; i < 100; i++){
-        append_string_c(string, '0' + (i%10));
-    }
-    TEST_ASSERT_EQUAL_STRING("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", string->data);
-
-    for (int i = 0; i < 200; i++) {
-        append_string_c(string, '0' + (i % 10));
-    }
-    TEST_ASSERT_EQUAL_STRING("012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", string->data);
-}
 
 int main(void) {
     UNITY_BEGIN();
-
-    // Basic tests
     RUN_TEST(test_init_string);
     RUN_TEST(test_free_string);
     RUN_TEST(test_append_string_to_empty);
-    RUN_TEST(test_append_string_char);
-
-    // Advanced tests
-    RUN_TEST(test_adv_append_long_string);
-    RUN_TEST(test_adv_append_string_char);
-
     return UNITY_END();
 }

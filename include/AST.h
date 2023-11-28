0e5f7be28b5c2bead7dfbf7e3645785c5b8cffb7
Ondřej Hruboš <hruboson@gmail.com>
2023-11-28 22:01:07 +0100

    Implementation of few first function for AST
    TODO implement all functions
    TODO create tests for AST

--- include/AST.h
+++ include/AST.h
@@ -6,9 +6,11 @@ typedef struct AST {
     Statement* statement;
 } AST;
 
-void init_ast( AST* );
-void clear_ast( AST* );
-void ast_append( AST*, Statement* );
-void ast_get( AST*, Statement* );
+void init_ast(AST*);
+void clear_ast(AST*);
+void ast_first(AST*, Statement*);
+void ast_append(AST*, Statement*);
+void ast_get(AST*, Statement*);
+void ast_delete_last(AST*);
 
-void print_ast( AST* );
+void print_ast(AST*);

e209606e7c0adca7337a141d70b6401dca465460
Hloody <sata@satinsky.cz>
2023-11-28 08:23:58 +0100

    added missing statements to struct Statement

--- include/statement.h
+++ include/statement.h
@@ -2,44 +2,57 @@
 
 #include "expression.h"
 #include "scanner.h"
+#include "table.h"
 
 typedef enum StatementType {
-    ST_VAR,
-    ST_IF,
-    ST_ASSIGN,
-    ST_WHILE,
-    ST_RETURN,
-    ST_EXP,
-    ST_FUNC
+	ST_VAR,
+	ST_IF,
+	ST_ASSIGN,
+	ST_WHILE,
+	ST_RETURN,
+	ST_EXP,
+	ST_FUNC
 } StatementType;
 
 typedef struct Statement {
-    StatementType type;
-    union {
-        struct {
-            bool modifiable;
-            SymbolRecord* id;
-            Expression* exp;
-            Keyword data_type;  // if KEYWORD_NIL -> not set
-            bool allow_nil;
-        } var;
-        struct {
-            SymbolRecord* id;
-            Expression* exp;
-        } assign;
-        struct {
-            Expression* exp;
-            bool check_nil;
-            struct Statement* body;
-            struct Statement* else_;
-        } if_;
-        struct {
-            Expression* exp;
-            struct Statement* body;
-        } while_;
-        struct {
-            Expression* exp;
-        } return_;
-    };
-    struct Statement* next;
+	StatementType type;
+	union {
+		struct {
+			bool modifiable;
+			SymbolRecord* id;
+			Expression* exp;
+			VarType data_type;  // if VARTYPE_VOID -> not set
+			bool allow_nil;
+			bool used;
+		} var;
+		struct {
+			SymbolRecord* id;
+			Expression* exp;
+		} assign;
+		struct {
+			Expression* exp;
+			bool check_nil;
+			struct Statement* body;
+			struct Statement* else_;
+		} if_;
+		struct {
+			Expression* exp;
+			struct Statement* body;
+		} while_;
+		struct {
+			Expression* exp;
+		} return_;
+		struct {
+			Expression* exp;
+		} exp;
+		struct {
+			SymbolRecord* id;
+			size_t param_count;
+			Parameter* parameters;
+			DataType return_type;
+			struct Statement* body;
+			bool used;
+		} func;
+	};
+	struct Statement* next;
 } Statement;

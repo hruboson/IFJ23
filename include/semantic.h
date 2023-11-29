e64f40f2a05c8598bdf0a411271d07de8225cedb
Dominik Borek <dominikborek@seznam.cz>
2023-11-29 15:37:57 +0100

    add another semantic analyze functions

--- include/semantic.h
+++ include/semantic.h
@@ -7,3 +7,9 @@
 // int semantic( Input*, AST* );
 
 bool semantic_variable(VarTableStack* stack, FuncTable* table, Statement* statement);
+bool semantic_assignment(VarTableStack *stack, FuncTable *table, Statement *statement);
+bool semantic_if(VarTableStack *stack, FuncTable *table, Statement *statement);
+bool semantic_while(VarTableStack *stack, FuncTable *table, Statement *statement);
+bool semantic_return(VarTableStack *stack, FuncTable *table, Statement *statement);
+bool semantic_expression(VarTableStack *stack, FuncTable *table, Statement *statement);
+bool semantic_function(VarTableStack *stack, FuncTable *table, Statement *statement);
--- src/semantic.c
+++ src/semantic.c
@@ -1,71 +1,110 @@
 #include "semantic.h"
 
-// var x = 5
-bool semantic_variable(VarTableStack *stack, FuncTable *table, Statement *statement)
-{
-    Variable *get_var = var_table_stack_get_var(&stack, statement->var.exp->id);
-    if (!get_var)
-    {
-        fprintf(stderr, "Chyba: %s (%s:%d): var_table_stack_get_var je neprázný\n", __func__, __FILE__, __LINE__);
-        return false;
+#define ERROR(msg) do { \
+    fprintf(stderr, "Chyba: %s (%s:%d): %s\n", __func__, __FILE__, __LINE__, msg); \
+    exit(9); \
+} while (0)
+
+#define SEMANTIC_ERROR_UNDEFINED_VARIABLE(id) do { \
+    fprintf(stderr, "Chyba: %s (%s:%d): Nedefinovaná proměnná '%s'\n", __func__, __FILE__, __LINE__, id); \
+    exit(5); \
+} while (0)
+
+#define SEMANTIC_ERROR_TYPE_MISMATCH do { \
+    fprintf(stderr, "Chyba: %s (%s:%d): Nesoulad typů mezi proměnnou a výrazem\n", __func__, __FILE__, __LINE__); \
+    exit(7); \
+} while (0)
+
+// Var x = 5
+bool semantic_variable(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    if (!stack) {
+        ERROR("Neplatný ukazatel na zásobník proměnných");
     }
 
-    switch (get_var->type.type){
-        
-        case VARTYPE_INT: // Pokud je proměnná typu int
-            if (statement->type == ET_INT){
-                //&statement->var.exp->int_; // v int_ bude uložena 5ka
-                get_var->initialized = true;
-                return true;
-            }
-            else{
-                fprintf(stderr, "Chyba: %s (%s:%d): nesoulad typů mezi proměnnou a výrazem\n", __func__, __FILE__, __LINE__);
-                return false;
-            }
-            break;
-        
-        case VARTYPE_DOUBLE: // Pokud je proměnná typu double
-            if (statement->type == ET_DOUBLE){
-                //&statement->var.exp->double_;
-                get_var->initialized = true;
-                return true;
-            }
-            else{
-                fprintf(stderr, "Chyba: %s (%s:%d): nesoulad typů mezi proměnnou a výrazem\n", __func__, __FILE__, __LINE__);
-                return false;
-            }
-            break;
-        
-        case VARTYPE_STRING: // Pokud je proměnná typu string
-            if (statement->type == ET_STRING) 
-            {
-                //&statement->var.exp->str_;
-                get_var->initialized = true;
-                return true;
-            }
-            else
-            {
-                fprintf(stderr, "Chyba: %s (%s:%d): nesoulad typů mezi proměnnou a výrazem\n", __func__, __FILE__, __LINE__);
-                return false;
-            }
-            break;
-        
-        case VARTYPE_VOID: // Pokud je proměnná typu void
-            if (statement->type == ET_STRING || statement->type == ET_INT || statement->type == ET_DOUBLE) // Zkontrolujte, zda je výraz typu string, int nebo double
-            {
-                get_var->initialized = true;
-                return true;
-            }
-            else 
-            {
-                fprintf(stderr, "Chyba: %s (%s:%d): nesoulad typů mezi proměnnou a výrazem\n", __func__, __FILE__, __LINE__);
-                return false;
-            }
-            break;
-        default: // Pokud je proměnná neznámého typu
-            fprintf(stderr, "Chyba: %s (%s:%d): neznámý typ proměnné\n", __func__, __FILE__, __LINE__);
-            return false;
-            break;
+    Variable *get_var = var_table_stack_get_var(stack, statement->var.exp->id);
+    if (!get_var) {
+        SEMANTIC_ERROR_UNDEFINED_VARIABLE(statement->var.exp->id);
     }
-    return false;
+
+    switch (get_var->type.type) {
+        case VARTYPE_INT:
+            if (statement->type != ET_INT) {
+                SEMANTIC_ERROR_TYPE_MISMATCH;
+            }
+            break;
+        case VARTYPE_DOUBLE:
+            if (statement->type != ET_DOUBLE) {
+                SEMANTIC_ERROR_TYPE_MISMATCH;
+            }
+            break;
+        case VARTYPE_STRING:
+            if (statement->type != ET_STRING) {
+                SEMANTIC_ERROR_TYPE_MISMATCH;
+            }
+            break;
+        case VARTYPE_VOID:
+            if (statement->type != ET_STRING && statement->type != ET_INT && statement->type != ET_DOUBLE) {
+                SEMANTIC_ERROR_TYPE_MISMATCH;
+            }
+            break;
+        default:
+            ERROR("Neznámý typ proměnné");
+    }
+
+    get_var->initialized = true;
+    return true;
+}
+
+// Assign x = y
+bool semantic_assignment(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    Variable *assigned_var = var_table_stack_get_var(stack, statement->assign.id);
+    if (!assigned_var) {
+        fprintf(stderr, "Chyba: %s (%s:%d): Nedefinovaná proměnná '%s'\n", __func__, __FILE__, __LINE__, statement->assign.id);
+        exit(5);
+    }
+
+    //TODO: doplnit případné další sémantické kontroly
+    //TODO: doplnit, co se stane při přiřazení
+    return true;
+}
+
+// If (condition) { ... } else { ... }
+bool semantic_if(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    //TODO: doplnit sémantickou analýzu pro if
+    //TODO: doplnit sémantickou analýzu pro kontrolu podmínky
+    //TODO: doplnit kontrolu, při if else
+
+    return true;
+}
+
+// While (condition) { ... }
+bool semantic_while(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    //TODO: doplnit sémantickou analýzu pro while
+    //TODO: doplnit sémantickou analýzu pro kontrolu podmínky
+
+    return true;
+}
+
+// Return expression;
+bool semantic_return(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    //TODO: doplnit sémantickou analýzu pro return
+    //TODO: kontrola správnosti výrazu návratové funkce a jeho typu
+
+    return true;
+}
+
+// Expression;
+bool semantic_expression(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    //TODO: bude sémantická analýza pro výraz? Popkud ano, doplnit
+
+    return true;
+}
+
+// Function declaration
+bool semantic_function(VarTableStack *stack, FuncTable *table, Statement *statement) {
+    //TODO: doplnit sémantikcou analýzu pro správnost deklarace funkce
+    //TODO: doplnit kontrolu, zda nebyla funkce již deklarována
+    //TODO: kontrola parametrů a návratových typů
+
+    return true;
 }

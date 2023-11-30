bdbb5e417c9e3a839967ec17054f34cad62d7f26
Ondřej Hruboš <hruboson@gmail.com>
2023-11-30 11:12:25 +0100

    Fixed AST and added AST tests

--- src/AST.c
+++ src/AST.c
@@ -1,4 +1,5 @@
 #include "AST.h"
+
 #include "stdlib.h";
 
 void init_ast(AST* ast) {
@@ -7,17 +8,24 @@ void init_ast(AST* ast) {
 
 void clear_ast(AST* ast) {
     if (ast->statement) {
-        Statement* next = ast->statement;
-        while (next) {
-            Statement* tmp = next->next;
-            free(next);
-            next = tmp;
-        }
+        /*Statement* curr = ast->statement;
+        while (curr) {
+            Statement* tmp = curr->next;
+            free(curr);
+            curr = tmp;
+        }*/
+		ast->statement = NULL;
     }
 }
 
 void ast_append(AST* ast, Statement* st) {
     Statement* curr = ast->statement;
+
+    if (!curr) {
+        ast->statement = st;
+        return;
+    }
+	
     while (curr->next) {
         curr = curr->next;
     }
@@ -36,10 +44,12 @@ void ast_get(AST* ast, Statement* st) {
 void ast_delete_last(AST* ast) {
     if (ast->statement) {
         Statement* curr = ast->statement;
+        Statement* prev = ast->statement;
         while (curr->next) {
+			prev = curr;
             curr = curr->next;
         }
-        free(curr);
+		prev->next = NULL;
     }
 }
 
--- tests/test_AST.c
+++ tests/test_AST.c
@@ -1,18 +1,121 @@
 #include "../include/AST.h"
 #include "unity.h"
 
+AST a_;
+AST* a = &a_;
+
 //! always needs to be defined
 void setUp() {
+    init_ast(a);
 }
 void tearDown() {
+    clear_ast(a);
 }
 
-void test_init(void) {
-    TEST_ASSERT(0 == "TODO TESTS FOR AST");
+void test_ast_init(void) {
+    AST init_;
+    AST* init = &init_;
+    init_ast(init);
+    TEST_ASSERT_NULL(init->statement);
+    clear_ast(init);
+}
+
+void test_ast_clear(void) {
+    AST clear_;
+    AST* clear = &clear_;
+    init_ast(clear);
+    TEST_ASSERT_NULL(clear->statement);
+
+    Statement st1;
+    st1.next = NULL;
+    Statement st2;
+    st2.next = NULL;
+    Statement st3;
+    st3.next = NULL;
+    Statement st4;
+    st4.next = NULL;
+    ast_append(clear, &st1);
+    TEST_ASSERT_NOT_NULL(clear->statement);
+
+    ast_append(clear, &st2);
+    ast_append(clear, &st3);
+    ast_append(clear, &st4);
+
+    clear_ast(clear);
+    TEST_ASSERT_NULL(clear->statement);
+}
+
+void test_ast_single_append(void) {
+    Statement st;
+    st.next = NULL;
+    ast_append(a, &st);
+    TEST_ASSERT_NOT_NULL(a->statement);
+    TEST_ASSERT_NULL(a->statement->next);
+}
+
+void test_ast_multiple_append(void) {
+    Statement st1;
+    st1.next = NULL;
+    Statement st2;
+    st2.next = NULL;
+    Statement st3;
+    st3.next = NULL;
+    ast_append(a, &st1);
+    ast_append(a, &st2);
+    ast_append(a, &st3);
+    TEST_ASSERT_NOT_NULL(a->statement);
+    TEST_ASSERT_NOT_NULL(a->statement->next);
+    TEST_ASSERT_NOT_NULL(a->statement->next->next);
+}
+
+void test_ast_first(void) {
+    Statement st1;
+    st1.next = NULL;
+    Statement st2;
+    st2.next = NULL;
+    Statement st3;
+    st3.next = NULL;
+    ast_append(a, &st1);
+    ast_append(a, &st2);
+    ast_append(a, &st3);
+    TEST_ASSERT_NOT_NULL(a->statement);
+    TEST_ASSERT_NOT_NULL(a->statement->next);
+    TEST_ASSERT_NOT_NULL(a->statement->next->next);
+
+    Statement* first;
+    ast_first(a, first);
+    TEST_ASSERT_EQUAL_PTR(&st1, first);
+}
+
+void test_ast_delete_last(void) {
+    Statement st1;
+    st1.next = NULL;
+    Statement st2;
+    st2.next = NULL;
+    Statement st3;
+    st3.next = NULL;
+    ast_append(a, &st1);
+    ast_append(a, &st2);
+    ast_append(a, &st3);
+    TEST_ASSERT_NOT_NULL(a->statement);
+    TEST_ASSERT_NOT_NULL(a->statement->next);
+    TEST_ASSERT_NOT_NULL(a->statement->next->next);
+
+    ast_delete_last(a);
+    TEST_ASSERT_NULL(a->statement->next->next);
+    ast_delete_last(a);
+    TEST_ASSERT_NULL(a->statement->next);
+    ast_delete_last(a);
+    TEST_ASSERT_NULL(a->statement);
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_init);
+    RUN_TEST(test_ast_init);
+    RUN_TEST(test_ast_clear);
+    RUN_TEST(test_ast_single_append);
+    RUN_TEST(test_ast_multiple_append);
+    RUN_TEST(test_ast_first);
+    RUN_TEST(test_ast_delete_last);
     return UNITY_END();
 }

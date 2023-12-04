40b333a5dfb02e2dba0d4686ac028f30ce3af55c
Ondřej Hruboš <hruboson@gmail.com>
2023-12-04 22:45:16 +0100

    Fixed definition of vartable_stack_pop and vartable_stack_peek

--- include/vartable_stack.h
+++ include/vartable_stack.h
@@ -15,8 +15,8 @@ void init_vartable_stack(VarTableStack* stack);
 void clear_vartable_stack(VarTableStack* stack);
 
 void vartable_stack_push(VarTableStack* stack, VarTable* vartable);
-void vartable_stack_pop(VarTableStack* stack, VarTable* vartable);
-void vartable_stack_peek(VarTableStack* stack, VarTable* vartable);
+void vartable_stack_pop(VarTableStack* stack, VarTable** vartable);
+void vartable_stack_peek(VarTableStack* stack, VarTable** vartable);
 
 Variable* var_table_stack_get_var(VarTableStack* stack, SymbolRecord* id); // wrong name (should be vartable_stack_get_var to keep it consistent)
 

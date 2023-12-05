5458bd83f800a35bcb740d50da7783cfd2dd87d1
Ondřej Hruboš <hruboson@gmail.com>
2023-12-05 09:22:48 +0100

    Fixed rule_tree implementation + added tests

--- src/rule_tree.c
+++ src/rule_tree.c
@@ -4,23 +4,31 @@
 #include "rules.h"
 #include "stdlib.h"
 
-void init_rule_tree(RuleNode* tree) {
-    tree->exp = NULL;
-    tree->left = NULL;
-    tree->right = NULL;
+void init_rule_tree(RuleNode** tree) {
+    (*tree) = (RuleNode*) malloc(sizeof(RuleNode));
+    (*tree)->exp = NULL;
+    (*tree)->left = NULL;
+    (*tree)->right = NULL;
 }
 void dispose_rule_tree(RuleNode* tree) {
+	//! fix last clear doesnt free the node (should probably be RuleNode** tree)
+    if (!tree) return;
     dispose_rule_tree(tree->left);
     dispose_rule_tree(tree->right);
+    free(tree);
 }
-void rule_tree_insert_left(RuleNode* root, RuleNode* rule) {
+void rule_tree_insert_left(RuleNode* root, Expression* exp) {
+    RuleNode* rule = malloc(sizeof(RuleNode));
     if (!rule) exit(99);
+    rule->exp = exp;
     rule->left = NULL;
     rule->right = NULL;
     root->left = rule;
 }
-void rule_tree_insert_right(RuleNode* root, RuleNode* rule) {
+void rule_tree_insert_right(RuleNode* root, Expression* exp) {
+    RuleNode* rule = malloc(sizeof(RuleNode));
     if (!rule) exit(99);
+    rule->exp = exp;
     rule->left = NULL;
     rule->right = NULL;
     root->right = rule;

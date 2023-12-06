76026baa40f440f899934f3b1e31c9f380538ac4
Hloody <sata@satinsky.cz>
2023-12-07 00:35:51 +0100

    kms

--- src/code_generator.c
+++ src/code_generator.c
@@ -150,13 +150,13 @@ i_asgn_nil( String* c, enum frame f, SymbolRecord* id ) {
 }
 
 void
-i_move( String* code, SymbolRecord* dst, SymbolRecord* src ) {
+i_move( String* code, enum frame f, SymbolRecord* dst, SymbolRecord* src ) {
 	enum frame f_dst = get_frame( dst );
 	enum frame f_src = get_frame( src );
 	string_append( code, "MOVE " );
 	append_var( code, f_dst, dst );
 	string_append( code, " " );
-	append_var( code, f_src, src );
+	append_var( code, f, src );
 	string_append( code, "\n" );
 }
 
@@ -438,7 +438,7 @@ append_ir_inst( String* code, const IR_Inst* i, enum frame f, const IR_Func* fn 
 		i_defvar( code, f, i->id );
 		break;
 	case IRT_asgn:
-		i_move( code, i->ops[ 0 ], i->ops[ 1 ] );
+		i_move( code, f, i->ops[ 0 ], i->ops[ 1 ] );
 		break;
 	case IRT_asgn_int:
 		i_defvar( code, f, i->a_i.id );
